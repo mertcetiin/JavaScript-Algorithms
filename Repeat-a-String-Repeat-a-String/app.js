64981b240fa9a6145be7276de50ef2a600831c29
Mert Çetin <102957602+mertcetiin@users.noreply.github.com>
2023-05-30 14:34:28 +0300

    Add files via upload

---
+++ Repeat-a-String-Repeat-a-String/app.js
@@ -0,0 +1,11 @@
+function repeatStringNumTimes(str, num) {
+
+    let accumulatedStr = '';
+
+    for (let i = 0; i < num; i++) {
+        accumulatedStr += str
+    }
+    return accumulatedStr;
+}
+
+alert(repeatStringNumTimes("abc", 3))
