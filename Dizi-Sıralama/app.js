4d37b01a36f5193a2108066f038d9b8815d913e2
Mert Çetin <102957602+mertcetiin@users.noreply.github.com>
2023-06-07 00:45:19 +0300

    Add files via upload

---
+++ Dizi-Sıralama/app.js
@@ -0,0 +1,10 @@
+// let arr = ['laptop', 'mouse', 'apple', 'keyboard', 'phone']
+
+// arr.sort().reverse();
+// alert(arr)
+
+
+let arr = [5, 25, 17, 24, 8]
+
+arr.sort(function (a, b) { return a - b })
+alert(arr)
