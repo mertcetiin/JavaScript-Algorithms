0200f2e172f5944628ee930e508b16789e2a4bba
Mert Çetin <102957602+mertcetiin@users.noreply.github.com>
2023-06-06 12:53:45 +0300

    Add files via upload

---
+++ Dİzide-Eleman-Arama/app.js
@@ -0,0 +1,22 @@
+let arr = ['apple', 'keyboard', 'mouse', 'phone', 'laptop']
+
+let sonuc = arr.includes('apple')
+
+if (sonuc == true) {
+    alert('Dizide var')
+} else {
+    alert('Dizide yok')
+}
+
+
+
+
+// let arr = ['apple', 'keyboard', 'mouse', 'phone', 'laptop']
+
+// let sonuc = arr.indexOf('apple')
+
+// if (sonuc != -1) {
+//     alert('Dizide var')
+// } else {
+//     alert('Dizide yok')
+// }
