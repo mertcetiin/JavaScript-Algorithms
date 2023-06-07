577e371ec829357cf784e19dc200b5057ccb5e4b
Mert Çetin <102957602+mertcetiin@users.noreply.github.com>
2023-06-07 13:57:24 +0300

    Update app.js

--- Dizideki-Değerlerin-Tersini-Al/app.js
+++ Dizideki-Değerlerin-Tersini-Al/app.js
@@ -1,15 +1,12 @@
 function DizininTersiniAl() {
     let arr = [-1, 2, -3];
+
     let tersi = [];
 
     for (let i = 0; i < arr.length; i++) {
-        let rakam = arr[i];
-        let tersRakam = -rakam;
-
-        tersi.push(tersRakam);
+        tersi.push(-(arr[i]));
     }
-
     return tersi;
 }
 
-alert(DizininTersiniAl());
+alert(DizininTersiniAl())
