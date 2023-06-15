295341b5ff58d9813df5af33cb65317eb07134f8
Mert Çetin <102957602+mertcetiin@users.noreply.github.com>
2023-06-15 15:59:35 +0300

    Update app.js

--- Harfler-Sayılar-ve-Hesaplama-İşlemi/app.js
+++ Harfler-Sayılar-ve-Hesaplama-İşlemi/app.js
@@ -1,6 +1,46 @@
 function sayilariTopla() {
     let sayi = 'gdf1dg1gf*4aP0';
+    let s1 = '';
+    let s2 = '';
+    let operator = null;
 
+    for (let i = 0; i < sayi.length; i++) {
+        let harf = sayi[i]
+
+        if (!isNaN(harf)) {
+            if (operator === null) {
+                s1 += harf;
+            } else {
+                s2 += harf;
+            }
+        } else if ('+-*/'.includes(harf)) {
+            operator = harf;
+        }
+    }
+    s1 = parseFloat(s1);
+    s2 = parseFloat(s2);
+
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = s1 + s2;
+            break;
+        case '-':
+            result = s1 - s2;
+            break;
+        case '*':
+            result = s1 * s2;
+            break;
+        case '/':
+            result = s1 / s2;
+            break;
+        default:
+            result = NaN;
+            break;
+    }
+    result = Math.round(result * 100) / 100;
+    return result;
 }
 
-//alert(sayilariTopla())
+alert(sayilariTopla())
