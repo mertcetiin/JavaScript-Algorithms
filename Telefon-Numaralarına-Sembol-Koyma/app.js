921b4fb8d04fc42e21fcae00d69e3de3fcfd07bd
Mert Çetin <102957602+mertcetiin@users.noreply.github.com>
2023-06-17 00:58:58 +0300

    Update app.js

---
+++ Telefon-Numaralarına-Sembol-Koyma/app.js
@@ -0,0 +1,20 @@
+function uygunTelNumarası() {
+    let verilenNumara = '3121887733';
+    let result = '';
+
+
+    let number = verilenNumara.split(' ');
+
+    for (let i = 0; i < number.length; i++) {
+        let num = number[i]
+
+        let ilkNum = num.slice(0, 3);
+        let ikinciNum = num.slice(3, 6);
+        let kalanNum = num.slice(6);
+
+        let result = '(' + ilkNum + ')' + ikinciNum + '-' + kalanNum
+        return result;
+    }
+}
+
+alert(uygunTelNumarası());
