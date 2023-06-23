dae6fef8007c79edefee1011c639a6da1f1cc9a9
Mert Çetin <102957602+mertcetiin@users.noreply.github.com>
2023-06-24 00:01:38 +0300

    Update app.js

--- Vücut-Kitle-Indeksi/app.js
+++ Vücut-Kitle-Indeksi/app.js
@@ -1,1 +1,27 @@
+// kilo / (boy * boy)
 
+const submitDOM = document.querySelector('.submit')
+
+submitDOM.addEventListener('click', () => {
+
+    let boy = document.querySelector('.boy').value
+    let kilo = document.querySelector('.kilo').value
+
+    let IndeksHesaplama = Math.floor(Number(kilo) / Number(boy * boy))
+
+    let durum = ''
+
+    if (IndeksHesaplama < 18) {
+        durum = 'Zayıf'
+    } else if (IndeksHesaplama >= 18 && IndeksHesaplama < 25) {
+        durum = 'Normal'
+    } else if (IndeksHesaplama >= 25 && IndeksHesaplama < 30) {
+        durum = 'Kilolu'
+    } else if (IndeksHesaplama >= 30 && IndeksHesaplama < 40) {
+        durum = 'Obez'
+    } else {
+        durum = 'İleri Derece Obez'
+    }
+
+    alert(`${durum}  ${IndeksHesaplama}`)
+})
